8a2ce7b4bd0ad872087847e2007abc6150ceefe3
Lucas <lucasrchaves1@gmail.com>
2019-11-12 21:38:58 -0300

    Commit inicial

---
+++ src/main/java/com/br/gestaoQVT/GestaoQvtApplication.java
@@ -0,0 +1,13 @@
+package com.br.gestaoQVT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoQvtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoQvtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/gestaoQVT/GestaoQvtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.gestaoQVT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoQvtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
